a40bec8e4450fd40600ca20c3eba67e3c0355186
danilo.medeiros <danilo.medeiros@splab.ufcg.edu.br>
2022-02-17 10:44:18 -0300

    added anime model

---
+++ src/models/Anime.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose');
+const User = require('./User');
+ 
+const animeSchema = new mongoose.Schema({
+    title: {
+        type: String,
+        required: true,
+        min: 6,
+        max: 255
+    },
+    creator: {
+        type: String,
+        required: true,
+        min: 6,
+        max: 255
+    },
+    year: {
+        type: Number,
+        required: true,
+    },
+    synopsis: {
+        type: String,
+        required: true,
+        max: 500
+    },
+    tags: {
+        type: [String],
+        required: false,
+    },
+    img: {
+        type: String,
+        required: false,
+        max: 500
+    },
+    user: {
+        type: User,
+        required: true
+    }
+});
+ 
+module.exports = mongoose.model('Anime', animeSchema);
