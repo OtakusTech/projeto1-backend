9fdd0e057067ddfecde0935ba54e08bdd9a71254
caiohrgm <caio.medeiros@ccc.ufcg.edu.br>
2022-02-23 10:31:20 -0300

    Atualizando user.js modelo

--- src/models/user.js
+++ src/models/user.js
@@ -1,15 +1,33 @@
 const mongoose = require('mongoose');
-
-const UserSchema = new mongoose.Schema(
-    {
-        date: {type: Date, default: Date.now},
-        name: {type: String, required: true, min: 6, max: 255},
-        email: {type: String, required: true, max: 255, min: 6},
-        password: {type: String, required: true, max: 1024, min: 6},
-        img: { type: String, required: false, max: 500}
+ 
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        min: 6,
+        max: 255
     },
-    {collection: 'users'}
-);
-
-//UserSchema.index({slug: 1, userid:1}, {unique: true});
-module.exports = mongoose.model('User', UserSchema);
+    email: {
+        type: String,
+        required: true,
+        max: 255,
+        min: 6
+    },
+    password: {
+        type: String,
+        required: true,
+        max: 1024,
+        min: 6
+    },
+    date: {
+        type: Date,
+        default: Date.now
+    },
+    img: {
+        type: String,
+        required: false,
+        max: 500
+    }
+});
+ 
+module.exports = mongoose.model('User', userSchema);
